638add76f8f177de6d15868fa6ee4e6732398a2e
Anurag Kumar <anurags62@yahoo.com>
2022-01-21 00:21:21 +0530

    c++

---
+++ Cpp_00_HackerRank/01_HelloWorld.cpp
@@ -0,0 +1,22 @@
+/*
+ * 01_HelloWorld.cpp
+ *
+ *  Created on: Jan 21, 2022
+ *      Author: anura
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+	cout<<"Hello To World"<<endl;
+
+
+	return 0;
+}
+
+
+
---
+++ Cpp_00_HackerRank/test.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : Test.cpp
+// Author      : anurag
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int main(void) {
+	puts("!!!Hello World!!!");
+	cout<<"hello"<<endl;
+	return EXIT_SUCCESS;
+}
