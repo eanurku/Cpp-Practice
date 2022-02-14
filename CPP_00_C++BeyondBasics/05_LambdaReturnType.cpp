3d340d19773b557b703977820ff2263000b81256
Anurag Kumar <anurags62@yahoo.com>
2022-02-15 00:18:02 +0530

    update

---
+++ CPP_00_C++BeyondBasics/05_LambdaReturnType.cpp
@@ -0,0 +1,32 @@
+/*
+ * 05_LambdaReturnType.cpp
+ *
+ *  Created on: Feb 14, 2022
+ *      Author: anura
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+using namespace std;
+
+int main() {
+
+	vector<int> list{1,2,3,4,5};
+
+	auto sum=accumulate(begin(list), end(list), 0,	[](auto total,auto e) -> double {
+		if(e%2==0)
+			return total+0.1;
+		else
+			return total+0;
+	});
+
+	cout<<"sum="<<sum<<endl;
+
+	return 0;
+}
+
+
+
+
