fe32895a595a4c4687386e53e9616ec456f52569
Anurag Kumar <anurags62@yahoo.com>
2022-09-15 14:44:52 +0530

    geek for geeks

--- CPP_01_C++ByGeeksForGeeks/02_ChronoDiffTime.cpp
+++ CPP_01_C++ByGeeksForGeeks/02_ChronoDiffTime.cpp
@@ -16,11 +16,26 @@
 #include <unordered_map>
 #include <sstream>
 #include <iomanip>
-
+#include <ctime>
+#include <chrono>
+#define LOOP(i,n) for(int i=0;i<=n;i++)
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl;
-	return 0;
+
+	std::chrono::time_point<std::chrono::system_clock> start,end;
+
+   start=std::chrono::system_clock::now();
+
+	LOOP(i,1000000000){
+		;
+	}
+
+	end=std::chrono::system_clock::now();
+
+	std::chrono::duration<double> time=end-start;
+	cout<<"chrono time="<<time.count()<<endl;
+
+	return EXIT_SUCCESS;
 }
 
