557c12e99ae20832eff0ac633a8d287cb5d8e0fc
Anurag Kumar <anurags62@yahoo.com>
2024-08-19 23:00:05 +0530

    update

--- CPP_ZEXP/src/CPP_ZEXP.cpp
+++ CPP_ZEXP/src/CPP_ZEXP.cpp
@@ -10,25 +10,65 @@
 #include <iostream>
 #include <algorithm>
 #include <chrono>
+#include <deque>
 
 using namespace std::chrono;
 using namespace std::chrono;
 using namespace std;
-int main(){
 
-	time_point<system_clock> start,end;
+void printMax(int array[],int n,int k){
+
+	deque<int> DQ;
+
+	for (int i = 0; i < k; ++i) {
+
+		if (DQ.empty()) {
+			DQ.push_back(i);
+		} else {
+
+			while (!DQ.empty() && array[i] > array[DQ.back()]) {
+				DQ.pop_back();
+			}
+			DQ.push_back(i);
+		}
+	}
 
 
-	start=system_clock::now();
+	for (int i = k; i < n; ++i) {
+		//print max of previous window
+		cout << array[DQ.front()] << " ";
+		//remove extra element of previous window
 
-	for (int var = 0; var < 1000000; ++var) {
+		while (!DQ.empty() && DQ.front() <= i - k) {
+			DQ.pop_front();
+		}
+
+		//insert element from back
+
+		while (!DQ.empty() && array[i] > array[DQ.back()]) {
+			DQ.pop_back();
+		}
+		DQ.push_back(i);
 
 	}
-	end=system_clock::now();
 
-	duration<double> time=end-start;
+	cout<<array[DQ.front()];
 
-	cout<<time.count();
 
+}
+int main(){
+
+	int n,k,array[100];
+
+	cin>>n>>k;
+	for (int i = 0; i < n; ++i) {
+		cin>>array[i];
+	}
+
+	for_each(begin(array), begin(array)+n, [](auto e){cout<<e<<" ";});
+
+	cout<<endl;
+
+	printMax(array,n,k);
 	return 0;
 }
